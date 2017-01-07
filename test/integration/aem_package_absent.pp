92fcc0e8743ad085a2c54a99310bb293f20c14ef
Cliffano Subagio <cliffano@gmail.com>
2017-01-07 18:26:30 +1100

    Fix delete package test description.

--- test/integration/aem_package_absent.pp
+++ test/integration/aem_package_absent.pp
@@ -1,4 +1,4 @@
-aem_package { 'Install AEM6.2 hotfix 12785':
+aem_package { 'Delete AEM6.2 hotfix 12785':
   ensure  => absent,
   name    => 'cq-6.2.0-hotfix-12785',
   group   => 'adobe/cq620/hotfix',
