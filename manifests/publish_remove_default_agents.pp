f192f9113ec47168619e492c0a1235955cb2c6a0
Cliffano Subagio <cliffano@gmail.com>
2017-01-26 10:16:45 +1100

    Add author/publish info on default agents removal to allow both author and publish default agents to be cleaned up from a single Puppet call.

--- manifests/publish_remove_default_agents.pp
+++ manifests/publish_remove_default_agents.pp
@@ -1,19 +1,19 @@
 class aem_resources::publish_remove_default_agents(
 ) {
 
-  aem_flush_agent { 'Delete flush agent - flush':
+  aem_flush_agent { 'Delete publish flush agent - flush':
     ensure   => absent,
     name     => 'flush',
     run_mode => 'publish',
   }
 
-  aem_replication_agent { 'Delete replication agent - outbox':
+  aem_replication_agent { 'Delete publish replication agent - outbox':
     ensure   => absent,
     name     => 'outbox',
     run_mode => 'publish',
   }
 
-  aem_flush_agent { 'Delete flush agent - resource-only-flush':
+  aem_flush_agent { 'Delete publish flush agent - resource-only-flush':
     ensure   => absent,
     name     => 'resource-only-flush',
     run_mode => 'publish',
