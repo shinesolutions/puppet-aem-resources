2d1ac43ad6594d25b12fe5345f74cd13e3bf749d
Cliffano Subagio <cliffano@gmail.com>
2017-01-27 22:01:05 +1100

    Change system users home dir from /system to /<first_char> since /system causes error 500.

--- manifests/create_system_users.pp
+++ manifests/create_system_users.pp
@@ -4,35 +4,35 @@ class aem_resources::create_system_users(
   aem_user { 'Create user - orchestrator':
     ensure   => present,
     name     => 'orchestrator',
-    path     => '/home/users/system',
+    path     => '/home/users/o',
     password => 'orchestrator'
   }
 
   aem_user { 'Create user - replicator':
     ensure   => present,
     name     => 'replicator',
-    path     => '/home/users/system',
+    path     => '/home/users/r',
     password => 'replicator'
   }
 
   aem_user { 'Create user - deployer':
     ensure   => present,
     name     => 'deployer',
-    path     => '/home/users/system',
+    path     => '/home/users/d',
     password => 'deployer'
   }
 
   aem_user { 'Create user - exporter':
     ensure   => present,
     name     => 'exporter',
-    path     => '/home/users/system',
+    path     => '/home/users/e',
     password => 'exporter'
   }
 
   aem_user { 'Create user - importer':
     ensure   => present,
     name     => 'importer',
-    path     => '/home/users/system',
+    path     => '/home/users/i',
     password => 'importer'
   }
 }
