37b424fa4faf2fb5a1334724cb3efcd612cab550
Cliffano Subagio <cliffano@gmail.com>
2017-01-30 12:08:35 +1100

    Fix lint violation.

--- test/integration/aem_publish_dispatcher_set_config.pp
+++ test/integration/aem_publish_dispatcher_set_config.pp
@@ -1,6 +1,6 @@
 class { 'aem_resources::publish_dispatcher_set_config':
-  conf_dir    => '/tmp/puppet-aem-resources/publish/',
-  docroot_dir => '/path/to/some/docroot/',
+  conf_dir     => '/tmp/puppet-aem-resources/publish/',
+  docroot_dir  => '/path/to/some/docroot/',
   publish_host => 'somepublishhost',
   publish_port => 4503
 }
