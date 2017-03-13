a6ba728b6c926614b47b368620e28cdbae4f4e22
Cliffano Subagio <cliffano@gmail.com>
2017-03-14 10:52:47 +1100

    Fix incorrect temp path.

--- test/integration/aem_author_dispatcher_set_config.pp
+++ test/integration/aem_author_dispatcher_set_config.pp
@@ -1,6 +1,6 @@
 class { 'aem_resources::author_dispatcher_set_config':
-  dispatcher_conf_dir => '/tmp/shinesolution/puppet-aem-resources/author-dispatcher/',
-  httpd_conf_dir      => '/tmp/shinesolution/puppet-aem-resources/author-httpd/',
+  dispatcher_conf_dir => '/tmp/shinesolutions/puppet-aem-resources/author-dispatcher/',
+  httpd_conf_dir      => '/tmp/shinesolutions/puppet-aem-resources/author-httpd/',
   docroot_dir         => '/path/to/some/docroot/',
   author_host         => 'someauthorhost',
   author_port         => 4502,
