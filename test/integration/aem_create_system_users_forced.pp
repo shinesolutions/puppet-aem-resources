bcb40bf1498c6d41283cff41b1dbdfd239227179
Cliffano Subagio <cliffano@gmail.com>
2017-03-17 00:03:05 +1100

    Fix lint indentation violation.

--- test/integration/aem_create_system_users_forced.pp
+++ test/integration/aem_create_system_users_forced.pp
@@ -1,7 +1,7 @@
 class { 'aem_resources::create_system_users':
   orchestrator_password => 'neworchestratorpassword',
-  replicator_password => 'newreplicatorpassword',
-  deployer_password => 'newdeployerpassword',
-  exporter_password => 'newexporterpassword',
-  importer_password => 'newimporterpassword',
+  replicator_password   => 'newreplicatorpassword',
+  deployer_password     => 'newdeployerpassword',
+  exporter_password     => 'newexporterpassword',
+  importer_password     => 'newimporterpassword',
 }
