c9c8789c4ac8adff5a3086974b6067eaf14ac7a3
Michael Bloch <michael.bloch@shinesolutions.com>
2018-03-20 13:55:37 +1100

    Updated test manifests.

--- test/integration/manifests/15_publish_dispatcher_set_config.pp
+++ test/integration/manifests/15_publish_dispatcher_set_config.pp
@@ -1,8 +1,10 @@
 aem_resources::publish_dispatcher_set_config { 'Set publish-dispatcher config':
   dispatcher_conf_dir => '/tmp/shinesolutions/puppet-aem-resources/publish-dispatcher/',
-  httpd_conf_dir      => '/tmp/shinesolutions/puppet-aem-resources/publish-httpd/',
+  apache_conf_dir     => '/tmp/shinesolutions/puppet-aem-resources/publish-httpd/',
   docroot_dir         => '/path/to/some/docroot/',
   allowed_client      => '*.*.*.*',
   publish_host        => 'somepublishhost',
   publish_port        => 4503,
+  publish_secure      => 1,
+  ssl_cert            => '/etc/ssl/aem.unified-dispatcher.cert
 }
