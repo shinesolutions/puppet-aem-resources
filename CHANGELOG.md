7eed547bd007e9e7282e369263c08de9f9b6a519
Michael Bloch <34461129+mbloch1986@users.noreply.github.com>
2018-03-27 21:46:52 +1100

    Fix 27/aem id handling (#28)
    * Added aem_id handling to deploy_packages
    
    For passing the aem_id variable to aem_pckage the aem_id handling needs
    to improve so it's accepting the aem_id injected from the provisioner.
    
    * Changelog update
    
    Regarding issue
    https://github.com/shinesolutions/puppet-aem-resources/issues/27
    
    * Fixed error of variable reassign
    
    * Updated aem_id handling from if else & selector to pick

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,5 @@
 ### 2.2.2
-*
+* Fix aem_id handling for deploy packages #27
 
 ### 2.2.1
 * Move puppet-aem-resources-generated httpd config to virtual_hosts_dir
--- manifests/deploy_packages.pp
+++ manifests/deploy_packages.pp
@@ -2,48 +2,53 @@ define aem_resources::deploy_packages (
   $packages,
   $path = '/tmp/shinesolutions/puppet-aem-resources',
   $sleep_seconds = 10,
-  $aem_username = undef,
-  $aem_password = undef,
+  $aem_id        = undef,
+  $aem_username  = undef,
+  $aem_password  = undef,
 ) {
 
   $packages.each | Integer $index, Hash $package| {
 
-    $aem_id = $package[aem_id]
+    $_aem_id = pick(
+      $package[aem_id],
+      $aem_id,
+      'author'
+      )
 
     $final_sleep_seconds = pick(
       $package['sleep_seconds'],
       $sleep_seconds,
     )
 
-    aem_package { "[${aem_id}] Deploy package ${package['group']}/${package['name']}-${package['version']}":
+    aem_package { "[${_aem_id}] Deploy package ${package['group']}/${package['name']}-${package['version']}":
       ensure       => present,
       name         => $package[name],
       group        => $package[group],
       version      => $package[version],
-      path         => "${path}/${aem_id}/${package['group']}",
+      path         => "${path}/${_aem_id}/${package['group']}",
       replicate    => $package[replicate],
       activate     => $package[activate],
       force        => $package[force],
       aem_username => $aem_username,
       aem_password => $aem_password,
-      aem_id       => $aem_id,
-    } -> exec { "[${aem_id}] Wait post Deploy package ${package['group']}/${package['name']}-${package['version']}":
+      aem_id       => $_aem_id,
+    } -> exec { "[${_aem_id}] Wait post Deploy package ${package['group']}/${package['name']}-${package['version']}":
       command     => "sleep ${final_sleep_seconds}",
       path        => ['/usr/bin', '/usr/sbin', '/bin'],
       timeout     => 0,
       refreshonly => true,
     }
 
-    aem_aem { "[${aem_id}] Wait until login page is ready post Deploy package ${package['group']}/${package['name']}-${package['version']}":
+    aem_aem { "[${_aem_id}] Wait until login page is ready post Deploy package ${package['group']}/${package['name']}-${package['version']}":
       ensure                     => login_page_is_ready,
       retries_max_tries          => 60,
       retries_base_sleep_seconds => 5,
       retries_max_sleep_seconds  => 5,
-      require                    => Exec["[${aem_id}] Wait post Deploy package ${package['group']}/${package['name']}-${package['version']}"],
+      require                    => Exec["[${_aem_id}] Wait post Deploy package ${package['group']}/${package['name']}-${package['version']}"],
       aem_username               => $aem_username,
       aem_password               => $aem_password,
-      aem_id                     => $aem_id,
-    } -> aem_aem { "[${aem_id}] Wait until aem health check is ok post Deploy package ${package['group']}/${package['name']}-${package['version']}":
+      aem_id                     => $_aem_id,
+    } -> aem_aem { "[${_aem_id}] Wait until aem health check is ok post Deploy package ${package['group']}/${package['name']}-${package['version']}":
       ensure                     => aem_health_check_is_ok,
       tags                       => 'deep',
       retries_max_tries          => 60,
@@ -51,7 +56,7 @@ define aem_resources::deploy_packages (
       retries_max_sleep_seconds  => 5,
       aem_username               => $aem_username,
       aem_password               => $aem_password,
-      aem_id                     => $aem_id,
+      aem_id                     => $_aem_id,
     }
 
   }
