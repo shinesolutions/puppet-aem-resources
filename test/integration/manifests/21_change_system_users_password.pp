b823e56beb9c94391bbe016a6a838b4e9cc4cb4d
Cliffano Subagio <cliffano@gmail.com>
2018-07-31 13:56:14 +1000

    Add doco on create system users and change password, move password params to aem_system_users hash, update integration tests.

--- test/integration/manifests/21_change_system_users_password.pp
+++ test/integration/manifests/21_change_system_users_password.pp
@@ -1,7 +1,36 @@
+$aem_system_users = {
+  deployer => {
+    name         => 'deployer',
+    path         => '/home/users/q',
+    old_password => 'deployer',
+    new_password => 'newdeployerpassword',
+  },
+  exporter => {
+    name         => 'exporter',
+    path         => '/home/users/e',
+    old_password => 'exporter',
+    new_password => 'newexporterpassword',
+  },
+  importer => {
+    name         => 'importer',
+    path         => '/home/users/i',
+    old_password => 'importer',
+    new_password => 'newimporterpassword',
+  },
+  orchestrator => {
+    name         => 'orchestrator',
+    path         => '/home/users/o',
+    old_password => 'orchestrator',
+    new_password => 'neworchestratorpassword',
+  },
+  replicator => {
+    name         => 'replicator',
+    path         => '/home/users/r',
+    old_password => 'replicator',
+    new_password => 'newreplicatorpassword',
+  }
+}
+
 aem_resources::change_system_users_password { 'Change system users password':
-  orchestrator_new_password => 'orchestrator',
-  replicator_new_password   => 'replicator',
-  deployer_new_password     => 'deployer',
-  exporter_new_password     => 'exporter',
-  importer_new_password     => 'importer',
+  aem_system_users => $aem_system_users,
 }
--- test/integration/manifests/22_create_system_users_forced.pp
+++ test/integration/manifests/22_create_system_users_forced.pp
@@ -1,7 +1,31 @@
+$aem_system_users = {
+  deployer => {
+    name     => 'deployer',
+    path     => '/home/users/q',
+    password => 'customdeployerpassword',
+  },
+  exporter => {
+    name     => 'exporter',
+    path     => '/home/users/e',
+    password => 'customexporterpassword',
+  },
+  importer => {
+    name     => 'importer',
+    path     => '/home/users/i',
+    password => 'customimporterpassword',
+  },
+  orchestrator => {
+    name     => 'orchestrator',
+    path     => '/home/users/o',
+    password => 'customorchestratorpassword',
+  },
+  replicator => {
+    name     => 'replicator',
+    path     => '/home/users/r',
+    password => 'customreplicatorpassword',
+  }
+}
+
 aem_resources::create_system_users { 'Create system users with predefined password':
-  orchestrator_password => 'neworchestratorpassword',
-  replicator_password   => 'newreplicatorpassword',
-  deployer_password     => 'newdeployerpassword',
-  exporter_password     => 'newexporterpassword',
-  importer_password     => 'newimporterpassword',
+  aem_system_users => $aem_system_users,
 }
