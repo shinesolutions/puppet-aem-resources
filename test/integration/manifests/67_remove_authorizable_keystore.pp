45232b1d9fe60f5feed772084d4ff95c1160e795
Cliffano Subagio <cliffano@gmail.com>
2018-11-23 17:03:22 +1100

    Fix Puppet manifest lint.

--- test/integration/manifests/67_remove_authorizable_keystore.pp
+++ test/integration/manifests/67_remove_authorizable_keystore.pp
@@ -1,4 +1,4 @@
 aem_resources::remove_authorizable_keystore { 'Remove authorizable Keystore':
-  authorizable_id                => 'authentication-service',
-  intermediate_path              => '/home/users/system',
+  authorizable_id   => 'authentication-service',
+  intermediate_path => '/home/users/system',
 }
