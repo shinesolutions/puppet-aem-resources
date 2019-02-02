bc9213232ed2d957cf18220eee5ebce354e8ecb5
Cliffano Subagio <cliffano@gmail.com>
2019-02-02 23:50:26 +1100

    Clean up lint violation.

--- test/integration/resources/01c_aem_aem_package_manager_is_ready.pp
+++ test/integration/resources/01c_aem_aem_package_manager_is_ready.pp
@@ -1,4 +1,4 @@
-aem_aem { "Wait until CRX Package Manager is ready":
+aem_aem { 'Wait until CRX Package Manager is ready':
   ensure                     => aem_package_manager_is_ready,
   retries_max_tries          => 60,
   retries_base_sleep_seconds => 5,
