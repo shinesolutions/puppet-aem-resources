f580ef036bad455c069849c2d41b16d037df6ecf
Carnifrex <30892449+Carnifrex@users.noreply.github.com>
2019-07-19 06:41:10 +0200

    Made the dispatcher farm template and httpd conf template source configurable so it can point to other template than the default one provided in aem_resources. (#66)

--- manifests/author_dispatcher_set_config.pp
+++ manifests/author_dispatcher_set_config.pp
@@ -6,6 +6,8 @@ define aem_resources::author_dispatcher_set_config(
   $author_port,
   $author_secure = '1',
   $ssl_cert = '/etc/httpd/aem.disp-cert',
+  $dispatcher_farm_template = 'aem_resources/author-dispatcher.farms.any.epp',
+  $httpd_conf_template = 'aem_resources/httpd.conf.epp',
 ) {
 
   $conf_dirs = [ $dispatcher_conf_dir, $virtual_hosts_dir ]
@@ -18,7 +20,7 @@ define aem_resources::author_dispatcher_set_config(
 
   file { "${dispatcher_conf_dir}/dispatcher.farms.any":
     ensure  => file,
-    content => epp('aem_resources/author-dispatcher.farms.any.epp', {
+    content => epp($dispatcher_farm_template, {
       author_host   => $author_host,
       author_port   => $author_port,
       author_secure => $author_secure,
@@ -30,7 +32,7 @@ define aem_resources::author_dispatcher_set_config(
 
   file { "${virtual_hosts_dir}/1-puppet-aem-resources.conf":
     ensure  => file,
-    content => epp('aem_resources/httpd.conf.epp', {
+    content => epp($httpd_conf_template, {
       docroot_dir => $docroot_dir,
       ssl_cert    => $ssl_cert,
     }),
