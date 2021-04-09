13b1bde7dfffd6cad8266043e3bbe618926e5291
Cliffano Subagio <cliffano@gmail.com>
2021-04-09 17:46:00 +1000

    Update copyright year and name.

--- lib/puppet/provider/aem_bundle/aem.rb
+++ lib/puppet/provider/aem_bundle/aem.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright 2016-2019 Shine Solutions
+# Copyright 2016-2021 Shine Solutions Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
